2eb1437f4367ed96d985443f0704d8fb1a374d53
EllieWalsch <elliewalsch4@gmail.com>
2023-03-17 16:09:34 -0500

    Populate the entire user model
    Just in case

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,12 +13,22 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation AddUser($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
         username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
       }
     }
   }
