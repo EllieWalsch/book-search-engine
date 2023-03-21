98eec25fabb85a833b23ea4eb736c5d36b613b75
EllieWalsch <elliewalsch4@gmail.com>
2023-03-21 17:57:12 -0500

    Change naming for simplicity

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -35,8 +35,8 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($input: SavedBook!) {
-    saveBook(input: $input) {
+  mutation saveBook($bookData: bookInput!) {
+    saveBook(bookData: $bookData) {
       _id
       bookCount
       email
